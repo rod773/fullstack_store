c0c6e8ec690b24eb44be1ed9c82ef01fc3a9d0b1
rod773 <rodrigoarcadio1@gmail.com>
2026-03-01 19:35:21 +0100

    server url

--- lib/constants/index.tsx
+++ lib/constants/index.tsx
@@ -2,3 +2,5 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "fullstack_store";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   "fullstack_store made width nextjs and shadcn";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
